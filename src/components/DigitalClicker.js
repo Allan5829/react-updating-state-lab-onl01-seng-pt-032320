a82163d67e80c145a6b7d8ba71b3a21e6fc6e810
Allan5829 <allan.farca@gmail.com>
2020-12-02 11:57:08 -0800

    current

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,27 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            timesClicked: 0
+        };
+    }
+
+    handleClick = () => {
+        this.setState(previousState => {
+          return {
+            count: previousState.count + 1
+          }
+        })
+    }
+   
+    render() {
+        return (
+        <button
+            onClick={this.handleClick}
+        />
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,42 @@
-// Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+export default class YouTubeDebugger extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                resolution: '1080p'
+                }
+            }
+        };
+    }
+
+    changeBitrate = () => {
+        this.setState(previousState => {
+          return {
+            count: previousState.count + 1
+          }
+        })
+    }
+
+    changeResolution = () => {
+        this.setState(previousState => {
+          return {
+            count: previousState.count + 1
+          }
+        })
+    }
+   
+    render() {
+        return (
+        <button
+            onClick={this.handleClick}
+        />
+        )
+    }
+}
